f05b9e2cb5b9ab43297a5b41b13c83f94efddc88
ChiniakinD <chiniakin.dmitrii@gmail.com>
2023-10-18 23:32:08 +0300

    Initial commit

---
+++ src/test/java/ru/liga/FoodService/OrderServiceTests.java
@@ -0,0 +1,13 @@
+package ru.liga.FoodService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
